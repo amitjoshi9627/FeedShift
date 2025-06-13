9c7fb3caf1cbcd4fd16e5e655047e24e6f457ec5
Amit Joshi <amit@Amits-MacBook-Pro.local>
2025-06-13 21:01:27 +0530

    [ADD] Version 0.2 updates

--- src/engine/run_engine.py
+++ src/engine/run_engine.py
@@ -3,6 +3,7 @@ from typing import Optional
 import pandas as pd
 
 from src.data.data_loader import FeedShiftDataLoader
+from src.ranking.constants import DEFAULT_TOXICITY_STRICTNESS
 from src.ranking.ranker import FeedShiftTextRanker
 
 
@@ -10,7 +11,9 @@ class FeedShiftEngine:
     def __init__(self, data: Optional[pd.DataFrame] = None) -> None:
         self.data_loader = FeedShiftDataLoader(data)
 
-    def run(self) -> pd.DataFrame:
+    def run(
+        self, toxicity_strictness: float = DEFAULT_TOXICITY_STRICTNESS
+    ) -> pd.DataFrame:
         text_ranker = FeedShiftTextRanker(self.data_loader.processed_data)
         return text_ranker.rerank()
 
--- src/ranking/ranker.py
+++ src/ranking/ranker.py
@@ -1,46 +1,50 @@
-from typing import Iterable
-
 import numpy as np
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.preprocessing import MinMaxScaler
 
 from src.data.constants import DataCols
+from src.models.detoxifier import FeedShiftDetoxified
 from src.models.embedder import FeedShiftEmbeddor
-from src.ranking.constants import RankingWeight
+from src.ranking.constants import DEFAULT_TOXICITY_STRICTNESS, RankingWeight
 
 
 class FeedShiftTextRanker:
     def __init__(self, data: pd.DataFrame):
         self.data = data
         self.dates = data[DataCols.TIMESTAMP]
+        self.texts = self.data[DataCols.TEXT].tolist()
         self.embeddor = FeedShiftEmbeddor()
+        self.detoxifier = FeedShiftDetoxified()
 
-    def rerank(self) -> pd.DataFrame:
-        embeddings = self._get_embeddings(self.data[DataCols.TEXT].tolist())
-        self.data[DataCols.SCORES] = self._get_score(self.dates, embeddings).round(1)
+    def rerank(
+        self, toxicity_strictness: float = DEFAULT_TOXICITY_STRICTNESS
+    ) -> pd.DataFrame:
+        self.data[DataCols.SCORES] = self._get_score(toxicity_strictness).round(1)
         self.data = self.data.sort_values(by=DataCols.SCORES, ascending=False)
         return self.data
 
-    def _get_embeddings(self, texts: list[str]) -> np.ndarray:
-        return self.embeddor(texts)
+    def _get_score(self, toxicity_strictness: float) -> np.ndarray:
+        uniqueness_score = self._get_uniqueness_score()
+        freshness_score = self._get_freshness_score()
+        toxicity_score = self._get_toxicity_score()
+        return (
+            RankingWeight.UNIQUENESS * uniqueness_score
+            + RankingWeight.FRESHNESS * freshness_score
+            + RankingWeight.TOXICITY * toxicity_strictness * toxicity_score
+        )
 
-    def _get_score(self, dates: Iterable[str], embeddings: np.ndarray) -> np.ndarray:
-        uniqueness_score = self._get_uniqueness_score(embeddings)
-        freshness_score = self._get_freshness_score(dates)
-        return RankingWeight.UNIQUENESS * uniqueness_score + RankingWeight.FRESHNESS * freshness_score
-
-    @staticmethod
-    def _get_uniqueness_score(embeddings: np.ndarray) -> np.ndarray:
+    def _get_uniqueness_score(self) -> np.ndarray:
+        embeddings = self.embeddor(self.texts)
         # Every point represent how much a sentence is similar to all other sentences
         similarity_score = np.mean(cosine_similarity(embeddings), axis=1)
         return 1 - MinMaxScaler().fit_transform(similarity_score.reshape(-1, 1))
 
-    @staticmethod
-    def _get_freshness_score(dates: Iterable[str]) -> np.ndarray:
-        dates = pd.to_datetime(dates)
+    def _get_freshness_score(self) -> np.ndarray:
+        dates = pd.to_datetime(self.dates)
         age_days = (dates.max() - dates).dt.seconds.values / (60 * 60 * 24)
         return 1 - MinMaxScaler().fit_transform(age_days.reshape(-1, 1))
 
-
-
+    def _get_toxicity_score(self) -> np.ndarray:
+        toxicity_scores = np.array(self.detoxifier.toxicity_score(self.texts))
+        return 1 - toxicity_scores
