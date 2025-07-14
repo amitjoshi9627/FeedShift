3c09998b43d23adccf08ab83e35ba92a42323617
Amit Joshi <amit@Amits-MacBook-Pro.local>
2025-07-14 15:43:37 +0530

    [UPDATE] Paths

--- src/config/paths.py
+++ src/config/paths.py
@@ -3,5 +3,5 @@ from pathlib import Path
 ROOT_DIR = Path(__file__).parents[2]
 
 DATA_DIR = ROOT_DIR / ".data_cache"
-RAW_DATA = DATA_DIR / "raw/raw_data.csv"
-PROCESSED_DATA = DATA_DIR / "processed/processed_data.csv"
+RAW_DATA_PATH = DATA_DIR / "raw/raw_data.csv"
+PROCESSED_DATA_PATH = DATA_DIR / "processed/processed_data.csv"
