b3f38a39ef86baee68ce91614f813207ab47af6d
Amit Joshi <amit@Amits-MacBook-Pro.local>
2025-07-14 15:37:57 +0530

    Added directory

--- src/engine/engine_factory.py
+++ src/engine/engine_factory.py
@@ -2,16 +2,19 @@ from typing import Optional
 
 import pandas as pd
 
-from src.data.data_loader import FeedShiftDataLoader
+from src.config.paths import RAW_DATA_PATH
+from src.data.constants import RedditDataCols
+from src.data.data_loader import FeedShiftDataLoader, RedditDataLoader, BaseDataLoader
+from src.data.preprocessing import RedditPreprocessor
 from src.ranking.constants import (
     DEFAULT_TOXICITY_STRICTNESS,
 )
-from src.ranking.ranker import FeedShiftTextRanker
+from src.ranking.ranker import TextRanker
 
 
 class FeedShiftEngine:
-    def __init__(self, data: Optional[pd.DataFrame] = None) -> None:
-        self.data_loader = FeedShiftDataLoader(data)
+    def __init__(self, path: str = RAW_DATA_PATH) -> None:
+        self.data_loader = RedditDataLoader(path)
 
     def run(
         self,
@@ -19,8 +22,10 @@ class FeedShiftEngine:
         toxicity_strictness: float = DEFAULT_TOXICITY_STRICTNESS,
         diversity_strength: float = 0.9,
     ) -> pd.DataFrame:
-        text_ranker = FeedShiftTextRanker(self.data_loader.processed_data)
+        text_ranker = TextRanker(self.data_loader.processed_data, timestamp_col=RedditDataCols.TIMESTAMP, text_col=RedditDataCols.PROCESSED_TITLE)
         return text_ranker.rerank(interests, toxicity_strictness, diversity_strength=diversity_strength)
+
+
 
 
 if __name__ == "__main__":
