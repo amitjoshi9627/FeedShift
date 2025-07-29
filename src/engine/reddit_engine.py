a7adfe1d4967ee571f3285614aeba23f93cc5b63
Amit Joshi <amit@Amits-MacBook-Pro.local>
2025-07-29 14:59:59 +0530

    [UPDATE] New Ingestion method - Reddit API

--- src/engine/reddit_engine.py
+++ src/engine/reddit_engine.py
@@ -1,21 +1,25 @@
-from pathlib import Path
+from abc import ABC
 
 import pandas as pd
 
 from src.data.constants import RedditDataCols
-from src.data.data_loader import RedditDataLoader
+from src.data.ingestors.reddit_ingestor import RedditIngestor
 from src.engine import BaseEngine
+from src.ranking.constants import DEFAULT_TOXICITY_STRICTNESS
 from src.ranking.ranker import TextRanker
 
 
-class RedditEngine(BaseEngine):
-    def __init__(self, path: str | Path | None) -> None:
-        super().__init__(path)
+class RedditEngine(BaseEngine, ABC):
+    def __init__(self, subreddit: str) -> None:
+        self.ingestor = RedditIngestor()
+        data = self.ingestor.ingest(subreddit)
+        self.text_ranker = TextRanker(data, timestamp_col=RedditDataCols.TIMESTAMP, text_col=RedditDataCols.TITLE)
 
-    @staticmethod
-    def _init_dataloader(path: str):
-        return RedditDataLoader(path)
-
-    @staticmethod
-    def _init_ranker(data: pd.DataFrame):
-        return TextRanker(data, timestamp_col=RedditDataCols.TIMESTAMP, text_col=RedditDataCols.TITLE)
+    def run(
+        self,
+        interests: list[str] | None = None,
+        toxicity_strictness: float = DEFAULT_TOXICITY_STRICTNESS,
+        diversity_strength: float = 0.9,
+    ) -> pd.DataFrame:
+        interests = interests if interests is not None else []
+        return self.text_ranker.rerank(interests, toxicity_strictness, diversity_strength=diversity_strength)
